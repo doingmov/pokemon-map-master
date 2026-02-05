133a3a7c967d1610581327c5f4aca1f7a93dc3d8
doingmov <yohei13111@mail.ru>
2026-02-05 19:26:15 +0300

    add specs to PokemonEntity model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -24,5 +24,11 @@ class PokemonEntity(models.Model):
 	appeared_at = models.DateTimeField(default=timezone.now)
 	disappeared_at = models.DateTimeField(default=timezone.now)
 
+	level = models.IntegerField(default=0)
+	health = models.IntegerField(default=0)
+	attack = models.IntegerField(default=0)
+	defense = models.IntegerField(default=0)
+	stamina = models.IntegerField(default=0)
+
 	def __str__(self):
 		return f"{self.pokemon.title} at ({self.lat}, {self.lon})"
