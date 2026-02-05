cbe8c20e271530ac64d5f733f4ac8c6bf427c32c
doingmov <yohei13111@mail.ru>
2026-02-05 23:28:56 +0300

    Add verbose_name to model fields for documentation and admin

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,34 +2,37 @@ from django.db import models
 from django.utils import timezone
 
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, verbose_name="Название")
     title_en = models.CharField(
     	max_length=200,
     	blank=True,
     	null=True,
-    	verbose_name="Name is English"
+    	verbose_name="Название (английский)"
     	)
     title_jp = models.CharField(
     	max_length=200,
     	blank=True,
     	null=True,
-    	verbose_name="Name is Japanese"
+    	verbose_name="Название (японский)"
     	)
     image = models.ImageField(
         upload_to='pokemon_images/',
         blank=True,
         null=True,
+        verbose_name="Изображение"
     )
     description = models.TextField(
         blank=True,
         null=True,
+        verbose_name="Описание"
     )
     previous_evolution = models.ForeignKey(
         'self',
         on_delete=models.SET_NULL,
         blank=True,
         null=True,
-        related_name='next_evolutions'
+        related_name='next_evolutions',
+        verbose_name="Из кого эволюционировал"
     )
 
 
@@ -42,18 +45,19 @@ class PokemonEntity(models.Model):
 	pokemon = models.ForeignKey(
 		Pokemon,
 		on_delete=models.CASCADE,
-		related_name='entities'
+		related_name='entities',
+		verbose_name="Покемон"
 		)
-	lat = models.FloatField()
-	lon = models.FloatField()
-	appeared_at = models.DateTimeField(default=timezone.now)
-	disappeared_at = models.DateTimeField(default=timezone.now)
+	lat = models.FloatField(verbose_name="Широта")
+	lon = models.FloatField(verbose_name="Долгота")
+	appeared_at = models.DateTimeField(default=timezone.now, verbose_name="Время появления")
+	disappeared_at = models.DateTimeField(default=timezone.now, verbose_name="Время исчезновения")
 
-	level = models.IntegerField(default=0)
-	health = models.IntegerField(default=0)
-	attack = models.IntegerField(default=0)
-	defense = models.IntegerField(default=0)
-	stamina = models.IntegerField(default=0)
+	level = models.IntegerField(default=0, verbose_name="Уровень")
+	health = models.IntegerField(default=0, verbose_name="Здоровье")
+	attack = models.IntegerField(default=0, verbose_name="Атака")
+	defense = models.IntegerField(default=0, verbose_name="Защита")
+	stamina = models.IntegerField(default=0, verbose_name="Выносливость")
 
 	def __str__(self):
 		return f"{self.pokemon.title} at ({self.lat}, {self.lon})"
