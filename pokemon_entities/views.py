19b0dd30e45d6d23606627cbed736878684478e5
doingmov <yohei13111@mail.ru>
2026-02-05 20:53:25 +0300

    show pokemons on mainpage map

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -50,8 +50,10 @@ def show_all_pokemons(request):
         })
 
     return render(request, 'mainpage.html', context={
+        'map': folium_map._repr_html_(),
         'pokemons': pokemons_on_page,
     })
+
 
 
 def show_pokemon(request, pokemon_id):
